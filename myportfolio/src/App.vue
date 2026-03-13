db833014e9226a1fe683b39f27cc462cd6717b1c
MRVanillaCoffee <misformhee@gmail.com>
2026-03-13 22:24:07 +0700

    FirstDraft

--- myportfolio/src/App.vue
+++ myportfolio/src/App.vue
@@ -1,11 +1,44 @@
-<script setup></script>
+<script>
+  export default{
+    name: 'app',
+    data(){
+      return{
+        firstName:'Thanakorn',
+        lastName:'Sertsai',
+        picture:'img/IMG_20260220_184928.jpg'
+      }
+    },
+
+    methods:{
+    getFullName(){
+      return this.firstName + ' ' + this.lastName
+    }
+  }
+  }
+
+  
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <section>
+    <nav class="navbar bg-dark">
+      <div class="container-fluid">
+            <a class="navbar-brand text-white" href="#"><h3>Thanakorn</h3></a>
+            <div>
+              <a class="navbar-brand text-white" href="#"><h3>Skill</h3></a>
+              <a class="navbar-brand text-white" href="#"><h3>About</h3></a>
+            </div>
+            
+      </div>
+    </nav>
+    <div class="container-fluid">
+
+      <h2>This website is {{ getFullName()}}'s portfolio</h2>
+      <img v-bind:src="picture" width="400">
+    </div>
+  </section>
 </template>
 
-<style scoped></style>
+<style>
+
+</style>
